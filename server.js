2d85b0c44deb1dc7daffd698df2413823119f17e
Rabyn-Collab <babynshrestha64@gmail.com>
2025-02-06 13:20:25 +0545

    changes

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', ['https://artist-management-livid.vercel.app']],
   credentials: true,
   // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   // allowedHeaders: ['Content-Type', 'Authorization']
