99d0ed48e420e0a129d75c8830c339080b82a829
Rabyn-Collab <babynshrestha64@gmail.com>
2025-02-06 13:16:09 +0545

    changes

--- artist-front/src/app/mainApi.js
+++ artist-front/src/app/mainApi.js
@@ -4,7 +4,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const mainApi = createApi({
   reducerPath: 'mainApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://192.168.1.73:5000/api',
+    baseUrl: 'https://artist-management-a3zi.onrender.com/api',
+    // baseUrl: 'http://192.168.1.73:5000/api',
     credentials: 'include',
   }),
   endpoints: (builder) => ({})
