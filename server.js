c839b7b04255e16ed6fb3b7ab2b24c84a5724f65
Rabyn-Collab <babynshrestha64@gmail.com>
2025-02-06 13:17:28 +0545

    changes

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ app.use(fileUpload({
 }));
 
 
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Server is running" });
+});
+
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/artists", artistRoutes);
